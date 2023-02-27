3394201c051438534c4e24663b46fe7703cebc6b
Pavan Kalyan Banala <94850864+pvnklyn@users.noreply.github.com>
2023-02-27 13:51:33 +0530

    Add files via upload

---
+++ prjt-2/app2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hello guys!!  👋 ")
+
+st.header(':blue[pvnklyn] Here :sunglasses:')
+st.subheader('This my first ever a web based data application !')
+
+
+st.write("## Connect me on Linkedin [link](https://www.linkedin.com/in/pavankalyanbanala)")
+st.write("## Follow me on Github [link](https://github.com/pvnklyn)")
